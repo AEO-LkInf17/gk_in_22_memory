b440649a6dfed23169e68f2dbfd24d52f9f35a4a
Lyncanette <100278980+Lyncanette@users.noreply.github.com>
2022-02-23 11:03:45 +0100

    Add files via upload
    das ist die Klasse Mr.Memeory

---
+++ MrMemeory.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+public class MrMemeory
+{
+  public void starteSpiel()
+  {
+    
+  }
+  public Spielbrett waehleSpielgroesse(Spielbrett sb) //waehlt Spielbrett
+  {
+    //übernimmt Layout eines Spielbretts
+  }
+  public void spielLaden() //oeffnet zuletzt gespeichertes Spiel
+  {
+    
+  }
+  public void spielSpeichern()
+  {
+    
+  }
+  public void aufgeben() //schliessen des Spiels ohne speichern
+  {
+    
+  }
+  public void statistikenAufrufen()
+  {
+    
+  }
+  public void optionenAufrufen()
+  {
+    
+  }
+  public void programmSchliessen()
+  {
+    
+  }
+  public void oeffneTrigger()
+  {
+    
+  }
+  public void musikLautstaerke()
+  {
+    
+  }
+  public void sfxLautstaerke()
+  {
+    
+  }
+  public boolean hellDunkel() //meint Light- bzw. Darkmode
+  {
+    
+  }
+  public void fensterSchliessen()
+  {
+    
+  }
+  public void oeffnePause()
+  {
+    
+  }
+}
