99114e16deebd48d082f5284c5e02d4a0c1acdf9
riiiiiiinzk <100278951+riiiiiiinzk@users.noreply.github.com>
2022-03-01 20:18:49 +0100

    arrays in einen extra ordner verschoben Test.java entfernt

---
+++ position aber ich weiß nicht mehr weiter/ZweiDimArrays.java
@@ -0,0 +1,30 @@
+public class ZweiDimArrays {
+  public static void main (String[] args) {
+    String [] [] feld = new String [4] [5]; //5x4 Brett
+    feld[0][0]="A1";
+    feld[0][1]="A2";
+    feld[0][2]="A3";
+    feld[0][3]="A4";
+    feld[0][4]="A5";
+    
+    feld[1][0]="B1";
+    feld[1][1]="B2";
+    feld[1][2]="B3";
+    feld[1][3]="B4";
+    feld[1][4]="B5";
+     
+    feld[2][0]="C1";
+    feld[2][1]="C2";
+    feld[2][2]="C3";
+    feld[2][3]="C4";
+    feld[2][4]="C5";
+     
+    feld[3][0]="D1";
+    feld[3][1]="D2";
+    feld[3][2]="D3";
+    feld[3][3]="D4";
+    feld[3][4]="D5";
+    
+     System.out.println(feld);
+  }
+}
